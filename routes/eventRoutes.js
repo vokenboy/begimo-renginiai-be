08ff78359f562b312cb75ba988642e16b35ec8bd
Vilius Tamašauskas <viliustam36@gmail.com>
2024-11-25 13:44:56 +0200

    Events functionality

---
+++ routes/eventRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const EventController = require('../controllers/eventController');
+
+const router = express.Router();
+
+router.get('/events', EventController.getAllEvents);
+router.get('/event/:id', EventController.getEventById);
+router.post('/event/postevent', EventController.createEvent);
+router.put('/event/editevent/:id', EventController.updateEventById);
+router.delete('/event/deleteevent/:id', EventController.deleteEventById);
+
+module.exports = router;
