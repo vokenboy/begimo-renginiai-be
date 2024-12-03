41c70b055af2ac5ab7b64f14bdd945fdaa7bbb1f
Be4nz <benas.andriuscenka@gmail.com>
2024-12-03 17:16:32 +0200

    Added some invite logic

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const cors = require('cors');
 const testRoute = require('./routes/testRoute');
 const userRoutes = require('./routes/userRoutes');
 const eventRoutes = require('./routes/eventRoutes');
+const inviteRoutes = require('./routes/inviteRoutes');
 require('dotenv').config();
 
 const app = express();
 const PORT = 5000;
 
-app.use(cors({
-  origin: 'http://localhost:3000'
-}));
+app.use(
+	cors({
+		origin: '*',
+	})
+);
 
 app.use(express.json());
 
@@ -18,7 +21,8 @@ app.use(express.json());
 app.use('/test', testRoute);
 app.use('/user', userRoutes);
 app.use('/event', eventRoutes);
+app.use('/invite', inviteRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+	console.log(`Server running on http://localhost:${PORT}`);
 });
